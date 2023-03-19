7e8a5717df33ea594efa44d179190c7118afcae7
Apro716 <89444135+APRO716@users.noreply.github.com>
2023-03-19 13:47:25 +0700

    hilo

--- source/Main.hx
+++ source/Main.hx
@@ -139,7 +139,7 @@ class Main extends Sprite
 		dateNow = dateNow.replace(" ", "_");
 		dateNow = dateNow.replace(":", "'");
 
-		path = "./crash/" + "PsychEngine_" + dateNow + ".txt";
+		path = "./crash/" + "HiloEngine_" + dateNow + ".txt";
 
 		for (stackItem in callStack)
 		{
